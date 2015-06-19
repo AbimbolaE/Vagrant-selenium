7a36fadbb980e66c8ac4eb7bf342ded577016ec2
Abimbola <abimbolaesuruoso@gmail.com>
2015-06-19 18:20:36 +0100

    Resolving the host OS's ip programatically and using it to register the node

--- firefox-node/boostrap.sh
+++ firefox-node/boostrap.sh
@@ -11,4 +11,5 @@ apt-get install -y openjdk-8-jdk
 wget http://selenium-release.storage.googleapis.com/2.46/selenium-server-standalone-2.46.0.jar
 
 #### Start up a node and point it to the grid on the host machine
-java -jar selenium-server-standalone-2.46.0.jar -role node -hub http://192.168.137.65:4444/grid/register
+host_ip=$(netstat -r | awk '/default/ {print $2}')
+java -jar selenium-server-standalone-2.46.0.jar -role node -hub http://$host_ip:4444/grid/register
